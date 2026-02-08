fa6688564807a1ec12f47f2054220cf48f751922
mohassan199319931993-dev <mohassan199319931993@gmail.com>
2026-02-08 15:08:55 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,6 @@
-const supabase = supabase.createClient(
+const supabaseClient.from('records').select('*')
   "https://PROJECT_ID.supabase.co",
   "ANON_PUBLIC_KEY"
 );
+
+window.supabaseClient = supabaseClient;
