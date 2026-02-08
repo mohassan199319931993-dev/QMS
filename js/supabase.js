a46f44d48e796c2c8a296c9ddc9d88978222ed3d
mohassan199319931993-dev <mohassan199319931993@gmail.com>
2026-02-08 15:11:00 +0200

    Set up Supabase client in supabase.js
    Initialize Supabase client for global access.

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,11 @@
-const supabaseClient.from('records').select('*')
+// نطلع createClient من المكتبة
+const { createClient } = supabaseJs;
+
+// ننشئ العميل
+const supabaseClient = createClient(
   "https://PROJECT_ID.supabase.co",
   "ANON_PUBLIC_KEY"
 );
 
+// نخليه Global عشان نستخدمه في أي ملف
 window.supabaseClient = supabaseClient;
